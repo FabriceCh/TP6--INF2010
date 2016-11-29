9b575e4e3742df9db1d68b6127f188c9e85c84d1
tristanmercille <tristanmercille@gmail.com>
2016-11-29 09:14:17 -0500

    tmb
    modifications apportee à findPath

--- Dijkstra.java
+++ Dijkstra.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+import java.util.Stack;
 
 public class Dijkstra {
 
@@ -25,27 +25,37 @@ public class Dijkstra {
 		dijkstraTable[0].put(edgeSS.getDestination(), edgeSS);
 			
 		Node selectedNode = s;
-		HashMap<Node,Edge> selectedNodes = new HashMap<Node,Edge>(); //Donne les noeuds choisis et leur valeur minimale pour se rendre
-		selectedNodes.put(s, edgeSS);
+		ArrayList<Node> selectedNodes = new ArrayList<Node>(); //Donne les noeuds choisis et leur valeur minimale pour se rendre
+		selectedNodes.add(selectedNode);
 		
-		for(int i = 1; i < graph.getNodes().size(); i++){
+		Stack<Node> path = new Stack<Node>();
+		path.push(selectedNode);
+		
+		int i = 1;
+		while (selectedNode != d){
 			dijkstraTable[i] = new HashMap<Node, Edge>(dijkstraTable[i-1]);
 			dijkstraTable[i].remove(selectedNode);
 			
 			for(Edge e : graph.getEdgesConnected(selectedNode)){
-				Edge eCopie = new Edge(e.getSource(), e.getDestination(), e.getDistance() + (dijkstraTable[i-1].get(selectedNode)).getDistance());
+				Edge connectedEdgePlus = new Edge(e.getSource(), e.getDestination(), e.getDistance() + (dijkstraTable[i-1].get(selectedNode)).getDistance());
 				
 				//Si le noeud a deja ete choisi, ou que la nouvelle valeur est superieure a celle deja dans la table de dijkstra, on ne fait rien
-				if(selectedNodes.containsKey(eCopie.getDestination()) || 
-						(dijkstraTable[i].containsKey(eCopie.getDestination()) && (eCopie.getDistance() > dijkstraTable[i].get(eCopie.getDestination()).getDistance()))){
+				if(selectedNodes.contains(connectedEdgePlus.getDestination()) || 
+						(dijkstraTable[i].containsKey(connectedEdgePlus.getDestination()) && (connectedEdgePlus.getDistance() > dijkstraTable[i].get(connectedEdgePlus.getDestination()).getDistance()))){
 				}
 				//sinon, on lajoute dans la table
 				else
-					dijkstraTable[i].put(eCopie.getDestination(), eCopie);
+					dijkstraTable[i].put(connectedEdgePlus.getDestination(), connectedEdgePlus);
 			}
 			
 			selectedNode = getMinimum(dijkstraTable[i]);
-			selectedNodes.put(selectedNode, dijkstraTable[i].get(selectedNode));
+			while(!path.empty() && path.peek() != dijkstraTable[i].get(selectedNode).getSource() ){
+				path.pop();
+			}
+			
+			path.push(selectedNode);
+			selectedNodes.add(selectedNode);
+			i++;
 		}
 
 	}
@@ -73,3 +83,44 @@ public class Dijkstra {
 		
 	}
 }
+
+/*
+public void findPath (Node s, Node d) {
+
+dijkstraTable = new HashMap[graph.getNodes().size()];
+
+dijkstraTable[0] = new HashMap<Node, Edge>();
+
+Stack<Node> path = new Stack<Node>();
+
+Edge edgeSS = new Edge(s,s,0);
+
+dijkstraTable[0].put(edgeSS.getDestination(), edgeSS);
+	
+Node selectedNode = s;
+HashMap<Node,Edge> selectedNodes = new HashMap<Node,Edge>(); //Donne les noeuds choisis et leur valeur minimale pour se rendre
+selectedNodes.put(s, edgeSS);
+
+for(int i = 1; i < graph.getNodes().size(); i++){
+	dijkstraTable[i] = new HashMap<Node, Edge>(dijkstraTable[i-1]);
+	dijkstraTable[i].remove(selectedNode);
+	
+	for(Edge e : graph.getEdgesConnected(selectedNode)){
+		Edge eCopie = new Edge(e.getSource(), e.getDestination(), e.getDistance() + (dijkstraTable[i-1].get(selectedNode)).getDistance());
+		
+		//Si le noeud a deja ete choisi, ou que la nouvelle valeur est superieure a celle deja dans la table de dijkstra, on ne fait rien
+		if(selectedNodes.containsKey(eCopie.getDestination()) || 
+				(dijkstraTable[i].containsKey(eCopie.getDestination()) && (eCopie.getDistance() > dijkstraTable[i].get(eCopie.getDestination()).getDistance()))){
+		}
+		//sinon, on lajoute dans la table
+		else
+			dijkstraTable[i].put(eCopie.getSource(), eCopie);
+	}
+	
+	selectedNode = getMinimum(dijkstraTable[i]);
+	path.push(selectedNode);
+	selectedNodes.put(selectedNode, dijkstraTable[i].get(selectedNode));
+}
+
+}
+*/
